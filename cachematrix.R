745a6b264a770b3bd570d1996f9ebed41d61ab14
greenorange04 <m-zolotko@yandex.ru>
2015-08-23 20:06:58 +0200

    Solution to programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix defines a list of four functions to set/get a matrix or its inverse(see below)
+## cacheSolve first looks if the  value of the inverse was already calculated and is available in the environment
+## if it is not available, it is calculated and "stored" via the function setinv
 
-## Write a short comment describing this function
+
+
+## The function creates a list of 4 functions to set and get the matrix and its inverse 
+## It is analogous to the makeVector function in the assignment instructions
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(calc_inv) inv <<- calc_inv
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function looks up if the value was already calculated and stored in the cache/environment. If 
+## yes, it returns the previously calculated value. If not, it calculates it and stores in the cache via setinv
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getinv()
+        if(!is.null(inv)){
+                return(inv)
+        }
+        
+        inv <- solve(x$get())
+        x$setinv(inv)
+        inv
+        
+        
+        
+        
+        
 }
